876811fc1732ca5061160e5d875543675263d930
ornette coleman <omniosi@gmail.com>
2015-10-22 15:51:17 -0700

    added dynamic test page. fixed back icon color.

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+include 'top-menu.html';
+// echo "Hello World";
+// needs a space here!
+// echo "Last Statement";
+
+// the script ends here with no PHP closing tag.
+// be sure to add a semicolon to end this php statement.
