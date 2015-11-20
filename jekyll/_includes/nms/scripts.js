2ff0ed5f231a90831942365b466c6521dd953ea0
ornette coleman <omniosi@gmail.com>
2015-11-20 13:02:09 -0800

    site variable setup. hover bug fix.

--- jekyll/_includes/nms/scripts.js
+++ jekyll/_includes/nms/scripts.js
@@ -3,7 +3,7 @@ $('.dot').on('mouseenter', function(){
 // $('.dot').on('hover', function(){
 // $('.dot').hover(function(){
 	// $(this).next('li').show(400);
-	$(this).next('li').fadeIn(300);
+	$(this).next('li').stop().fadeIn(300);
 	// $(this).next('li').fadeIn(300).prev('li').on('mouseout',function(){});
 	// $(this).next('li').fadeIn(300, function(){
 		// $(this).on('mouseout'.fadeOut(300);
@@ -18,7 +18,7 @@ $('.dot').on('mouseenter', function(){
 	// $(this).find('.pop').fadeIn(300);
 });
 $('.dot').on('mouseout', function(){
-	$(this).next('li').fadeOut(300);
+	$(this).next('li').stop().fadeOut(300);
 });
 // $('.pop').on('mouseout', function(){
 // 	$(this).fadeOut(300);
